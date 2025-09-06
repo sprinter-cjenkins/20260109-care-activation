7037b7cf8a3e0e5233f88a5c192e19318803d497
Chris Jenkins <christopher.jenkins.contractor@sprinterhealth.com>
2025-09-05 19:00:38 -0500

    Report Summary
    ┌──────────────────────────────────┬───────────┬───────────────────┐
    │              Target              │   Type    │ Misconfigurations │
    ├──────────────────────────────────┼───────────┼───────────────────┤
    │ .                                │ terraform │         0         │
    ├──────────────────────────────────┼───────────┼───────────────────┤
    │ ../../modules/networking/main.tf │ terraform │         3         │
    └──────────────────────────────────┴───────────┴───────────────────┘

--- infra/terraform/environment/dev/aws-networking.tf
+++ infra/terraform/environment/dev/aws-networking.tf
@@ -10,17 +10,17 @@ module "networking" {
     "public-subnet-1" = {
       cidr_block              = cidrsubnet(var.vpc_cidr, 8, 0)
       availability_zone       = data.aws_availability_zones.available.names[0]
-      map_public_ip_on_launch = true
+      map_public_ip_on_launch = false
     }
     "public-subnet-2" = {
       cidr_block              = cidrsubnet(var.vpc_cidr, 8, 1)
       availability_zone       = data.aws_availability_zones.available.names[1]
-      map_public_ip_on_launch = true
+      map_public_ip_on_launch = false
     }
     "public-subnet-3" = {
       cidr_block              = cidrsubnet(var.vpc_cidr, 8, 2)
       availability_zone       = data.aws_availability_zones.available.names[2]
-      map_public_ip_on_launch = true
+      map_public_ip_on_launch = false
     }
     "private-subnet-1" = {
       cidr_block              = cidrsubnet(var.vpc_cidr, 8, 100)
