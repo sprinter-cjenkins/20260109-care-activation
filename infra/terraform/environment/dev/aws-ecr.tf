3d27767e148fa1978255a2b6fca6f49a6b1d5cad
Chris Jenkins <christopher.jenkins.contractor@sprinterhealth.com>
2025-09-19 15:10:00 -0500

    update

--- infra/terraform/environment/dev/aws-ecr.tf
+++ infra/terraform/environment/dev/aws-ecr.tf
@@ -1,4 +1,35 @@
-resource "aws_ecr_repository" "care-activation" {
+# IAM Role
+resource "aws_iam_role" "ca_ecr_rw_role" {
+  name                 = "${var.resource_name}-${terraform.workspace}-ecr-rw-role"
+  path                 = "/"
+  max_session_duration = 3600
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:Pulsetera/${var.resource_name}:*"
+          }
+          StringNotEquals = {
+            "token.actions.githubusercontent.com:sub" = "repo:Pulsetera/${var.resource_name}:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_ecr_repository" "care_activation" {
   image_tag_mutability = "MUTABLE"
   name                 = "care-activation"
 
@@ -7,19 +38,19 @@ resource "aws_ecr_repository" "care-activation" {
   }
 }
 
-resource "aws_ecr_lifecycle_policy" "care-activation" {
-  repository = aws_ecr_repository.care-activation.name
+resource "aws_ecr_lifecycle_policy" "care_activation" {
+  repository = aws_ecr_repository.care_activation.name
 
   policy = jsonencode({
     rules = [
       {
         rulePriority = 1
-        description  = "Prune untagged images."
+        description  = "Keep last 16 release images"
         selection = {
-          tagStatus   = "untagged"
-          countType   = "sinceImagePushed"
-          countUnit   = "days"
-          countNumber = 1
+          tagStatus     = "tagged"
+          tagPrefixList = ["v-care-activation"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 16
         }
         action = {
           type = "expire"
@@ -27,12 +58,12 @@ resource "aws_ecr_lifecycle_policy" "care-activation" {
       },
       {
         rulePriority = 2
-        description  = "Prune old release images."
+        description  = "Keep last 8 pre-release images"
         selection = {
-          tagStatus      = "tagged"
-          tagPrefixList  = ["v-care-activation-build-", "v-care-activation-commit-"]
-          countType      = "imageCountMoreThan"
-          countNumber    = 16
+          tagStatus     = "tagged"
+          tagPrefixList = ["pre-care-activation"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 8
         }
         action = {
           type = "expire"
@@ -40,11 +71,11 @@ resource "aws_ecr_lifecycle_policy" "care-activation" {
       },
       {
         rulePriority = 3
-        description  = "Catch-all rule: keep last 16 images for any other tags."
+        description  = "Catch-all for commit builds"
         selection = {
-          tagStatus   = "tagged"
+          tagStatus   = "any"
           countType   = "imageCountMoreThan"
-          countNumber = 16
+          countNumber = 64
         }
         action = {
           type = "expire"
@@ -53,3 +84,4 @@ resource "aws_ecr_lifecycle_policy" "care-activation" {
     ]
   })
 }
+
