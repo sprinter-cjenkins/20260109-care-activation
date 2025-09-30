0a9b67f6bdfb445b27dc2a241fe7c689ccdfff13
Sailesh Kumar <sailesh@sprinterhealth.com>
2025-09-30 16:59:45 -0500

    lint

--- www/src/caller/caller.service.ts
+++ www/src/caller/caller.service.ts
@@ -9,7 +9,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { CallResult, CallerProvider } from './providers/caller-provider';
 import { BlandAIProvider } from './providers/bland-ai.provider';
 import type { Request } from 'express';
-import { ElevenLabsProvider } from './providers/eleven-labs.provider';
+// import { ElevenLabsProvider } from './providers/eleven-labs.provider';
 
 export interface APICallResult extends CallResult {
   message: string;
